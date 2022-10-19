35706c9af5ee4fc27e0c33463d17f190fc8426ee
HARI-PRMD <100102134+HARI-PRMD@users.noreply.github.com>
2022-10-19 19:15:22 +1100

    Hari youtube embed widget basic dragging functionality (#29)
    * added basic dragging functionality and styling to widget
    
    * changing branch name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { YoutubeEmbed } from "./components/YoutubeEmbed";
 
 function App() {
-  return <Navbar />;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="work-area">
+        <YoutubeEmbed />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCirclePlay,
   faClock,
@@ -7,6 +8,95 @@ import {
   faSun,
   faUser,
 } from "@fortawesome/free-regular-svg-icons";
+
+import { useState } from "react";
+
+interface buttonStates {
+  clock: boolean;
+  todo: boolean;
+  notes: boolean;
+  play: boolean;
+  setting: boolean;
+  user: boolean;
+}
+
+const Navbar = () => {
+  const [toggleBtn, setToggleBtn] = useState<buttonStates>({
+    clock: false,
+    todo: false,
+    notes: false,
+    play: false,
+    setting: false,
+    user: false,
+  });
+
+  function handleOnClick(key: keyof buttonStates) {
+    const existingState = { ...toggleBtn };
+    existingState[key] = !existingState[key];
+    setToggleBtn(existingState);
+  }
+
+  return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <span>
+          <FontAwesomeIcon
+            className="btn"
+            style={{ opacity: toggleBtn.clock ? 1 : 0.7 }}
+            onClick={() => handleOnClick("clock")}
+            icon={faClock}
+            size="lg"
+          />
+        </span>
+        <span>
+          <FontAwesomeIcon
+            className="btn"
+            style={{ opacity: toggleBtn.todo ? 1 : 0.7 }}
+            onClick={() => handleOnClick("todo")}
+            icon={faSquareCheck}
+            size="lg"
+          />
+        </span>
+        <span>
+          <FontAwesomeIcon
+            className="btn"
+            style={{ opacity: toggleBtn.notes ? 1 : 0.7 }}
+            onClick={() => handleOnClick("notes")}
+            icon={faStickyNote}
+            size="lg"
+          />
+        </span>
+        <span>
+          <FontAwesomeIcon
+            className="btn"
+            style={{ opacity: toggleBtn.play ? 1 : 0.7 }}
+            onClick={() => handleOnClick("play")}
+            icon={faCirclePlay}
+            size="lg"
+          />
+        </span>
+      </div>
+
+      <div className="navbar-right">
+        <span>
+          <FontAwesomeIcon
+            className="btn"
+            style={{ opacity: toggleBtn.setting ? 1 : 0.7 }}
+            onClick={() => handleOnClick("setting")}
+            icon={faSun}
+            size="lg"
+          />
+        </span>
+        <span>
+          <FontAwesomeIcon
+            className="btn"
+            style={{ opacity: toggleBtn.user ? 1 : 0.7 }}
+            onClick={() => handleOnClick("user")}
+            icon={faUser}
+            size="lg"
+          />
+        </span>
+
 import React from "react";
 import NavbarButton from "./NavbarButton";
 
