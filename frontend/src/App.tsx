d23d65ea364e32b61ef33c743ba81f21826d90f1
Merry Rosalie <z5391208@ad.unsw.edu.au>
2022-10-31 01:43:58 +1100

    Undo moving logic to GlobalStyles.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,21 @@ import Navbar from "./components/navbar/Navbar";
 import YoutubeEmbed from "./components/youtube/YoutubeEmbed";
 import { Fragment } from "react";
 import Settings from "./components/themes/Settings";
-import GlobalStyles from "./GlobalStyles";
+import { createGlobalStyle } from 'styled-components';
+import { useRecoilValue } from "recoil";
+import { globalStyles } from "./recoil_state";
 
 function App() {
+  const currentGlobalStyles = useRecoilValue(globalStyles);
+
+  const GlobalStyles = createGlobalStyle`
+    :root {
+      font-family: ${currentGlobalStyles.fontFamily};
+      background-image: ${currentGlobalStyles.backgroundImage};
+      color-scheme: ${currentGlobalStyles.theme} !important
+    }
+  `;
+  
   return (
     <Fragment>
       <GlobalStyles />
@@ -17,4 +29,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
