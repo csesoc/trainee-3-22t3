34ff48c48f73dc1cadcc0162af0a5950c0cc51b0
Merry Rosalie <z5391208@ad.unsw.edu.au>
2022-10-31 00:54:22 +1100

    Converted tabs to spaces

--- frontend/src/components/themes/FontItem.tsx
+++ frontend/src/components/themes/FontItem.tsx
@@ -3,24 +3,24 @@ import { globalStyles } from '../../recoil_state';
 import { FontItemDiv } from './SettingsStyled';
 
 interface FontItemProps {
-	fontFamily: string
+  fontFamily: string
 }
 
 const FontItem = (props: FontItemProps) => {
-	const [font, setFont] = useRecoilState(globalStyles);
-	
-	const changeFont = () => {
-		const currentStyles = { ...font };
-		currentStyles.fontFamily = props.fontFamily;
-		setFont(currentStyles);
-	}
-	
-	return (
-		<FontItemDiv onClick={changeFont} fontFamily={props.fontFamily}>
-			<h2>Aa</h2>
-			<p>{props.fontFamily}</p>
-		</FontItemDiv>
-	)
+  const [font, setFont] = useRecoilState(globalStyles);
+  
+  const changeFont = () => {
+    const currentStyles = { ...font };
+    currentStyles.fontFamily = props.fontFamily;
+    setFont(currentStyles);
+  }
+  
+  return (
+    <FontItemDiv onClick={changeFont} fontFamily={props.fontFamily}>
+      <h2>Aa</h2>
+      <p>{props.fontFamily}</p>
+    </FontItemDiv>
+  )
 }
 
 export default FontItem;
--- frontend/src/components/themes/Theme.tsx
+++ frontend/src/components/themes/Theme.tsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	faMoon,
-	faStar,
+  faMoon,
+  faStar,
 } from "@fortawesome/free-regular-svg-icons";
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { globalStyles } from "../../recoil_state";
@@ -9,62 +9,62 @@ import { useRecoilState } from "recoil";
 import { CustomBgForm, ThemeCatalogueDiv, ThemeItemDiv } from "./SettingsStyled";
 
 interface TargetAdditionalKey {
-	customUrl: {
-		value: string
-	}
+  customUrl: {
+    value: string
+  }
 }
 
 type ChangeBackgroundFormInterface = EventTarget & TargetAdditionalKey;
 
 async function checkImageUrl(url: string){
-	return new Promise((resolve) => {
-		const img = new Image();
-		img.src = url;
-		img.onload = () => resolve(true);
-		img.onerror = () => resolve(false);
-	});
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = url;
+    img.onload = () => resolve(true);
+    img.onerror = () => resolve(false);
+  });
 }
 
 const Theme = () => {
-	const [mode, setMode] = useRecoilState(globalStyles);
+  const [mode, setMode] = useRecoilState(globalStyles);
 
-	const changeMode = (modeString: 'light' | 'dark') => {
-		const currentStyles = { ...mode };
-		if (currentStyles.theme !== modeString) {
-			currentStyles.theme = modeString;
-			setMode(currentStyles);
-		}
-	}
+  const changeMode = (modeString: 'light' | 'dark') => {
+    const currentStyles = { ...mode };
+    if (currentStyles.theme !== modeString) {
+      currentStyles.theme = modeString;
+      setMode(currentStyles);
+    }
+  }
 
-	const handleOnSubmit : any = async (event: Event) => {
-		event.preventDefault();
-		const currentStyles = { ...mode };
-		const customUrl = (event.target as ChangeBackgroundFormInterface).customUrl.value;
-		if (await checkImageUrl(customUrl)) {
-			currentStyles.backgroundImage = `url(${customUrl})`;
-			setMode(currentStyles);
-		}
-	}
+  const handleOnSubmit : any = async (event: Event) => {
+    event.preventDefault();
+    const currentStyles = { ...mode };
+    const customUrl = (event.target as ChangeBackgroundFormInterface).customUrl.value;
+    if (await checkImageUrl(customUrl)) {
+      currentStyles.backgroundImage = `url(${customUrl})`;
+      setMode(currentStyles);
+    }
+  }
 
-	return (
-		<div>
-			<h1>Themes</h1>
-			<CustomBgForm onSubmit={handleOnSubmit}>
-				<input name="customUrl" type="text" placeholder='Input image URL...'/>
-				<button><FontAwesomeIcon icon={faSearch} /></button>
-			</CustomBgForm>
-			<ThemeCatalogueDiv>
-				<ThemeItemDiv onClick={() => changeMode('dark')}>
-					<FontAwesomeIcon icon={faMoon} />
-					<p>Dark Mode</p>
-				</ThemeItemDiv>
-				<ThemeItemDiv onClick={() => changeMode('light')}>
-					<FontAwesomeIcon icon={faStar} />
-					<p>Light Mode</p>
-				</ThemeItemDiv>
-			</ThemeCatalogueDiv>
-		</div>
-	)
+  return (
+    <div>
+      <h1>Themes</h1>
+      <CustomBgForm onSubmit={handleOnSubmit}>
+        <input name="customUrl" type="text" placeholder='Input image URL...'/>
+        <button><FontAwesomeIcon icon={faSearch} /></button>
+      </CustomBgForm>
+      <ThemeCatalogueDiv>
+        <ThemeItemDiv onClick={() => changeMode('dark')}>
+          <FontAwesomeIcon icon={faMoon} />
+          <p>Dark Mode</p>
+        </ThemeItemDiv>
+        <ThemeItemDiv onClick={() => changeMode('light')}>
+          <FontAwesomeIcon icon={faStar} />
+          <p>Light Mode</p>
+        </ThemeItemDiv>
+      </ThemeCatalogueDiv>
+    </div>
+  )
 }
 
 export default Theme;
