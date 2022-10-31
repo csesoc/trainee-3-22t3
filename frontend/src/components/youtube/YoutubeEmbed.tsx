a809308e1ce261e2cf562cd9e304bca37df15c80
Hari Pramod <haripramodb@gmail.com>
2022-10-31 20:33:02 +1100

    made youtube widget responsive

--- frontend/src/components/youtube/YoutubeEmbed.tsx
+++ frontend/src/components/youtube/YoutubeEmbed.tsx
@@ -1,25 +1,25 @@
-import React from "react"
-import "./YoutubeEmbed.css"
-import Draggable from "react-draggable"
+import React from "react";
+import "./YoutubeEmbed.css";
+import Draggable from "react-draggable";
 
 const YoutubeEmbed = () => {
   return (
     <Draggable>
-      <div className='youtube-widget'>
-        <div>
+      <div className="youtube-widget">
+        <div className="video-container">
           <iframe
-            width='854'
-            height='480'
-            src='https://www.youtube.com/embed/NvftPSb5Xtw'
-            title='Secret Forest 🍃 Chill Lofi Beats'
-            frameBorder='0'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            width="854"
+            height="480"
+            src="https://www.youtube.com/embed/NvftPSb5Xtw"
+            title="Secret Forest 🍃 Chill Lofi Beats"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
         </div>
       </div>
     </Draggable>
-  )
-}
+  );
+};
 
-export default YoutubeEmbed
+export default YoutubeEmbed;
