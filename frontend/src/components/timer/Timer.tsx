7dc364d0a76ffcbc8157cb80d5bb4ac7d6137966
Joshi <joshi.ha7@gmail.com>
2022-12-08 17:17:33 +1100

    Uncommenting code and with a few bug fixes.

--- frontend/src/components/timer/Timer.tsx
+++ frontend/src/components/timer/Timer.tsx
@@ -118,7 +118,7 @@ const PomodoroTimer = () => {
         </div>
       </Timer>
 
-      {/* {openSettings ? <TimerSettings /> : <></>} */}
+      {openSettings ? <TimerSettings /> : <></>}
     </>
   );
 };
